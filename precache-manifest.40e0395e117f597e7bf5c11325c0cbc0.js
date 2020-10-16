e3d3a556ea2c0ca21530da08e75bf6e98c1f9c2d
Hanh Nguyen <hanhn@Hanhs-MBP.fios-router.home>
2020-10-16 12:05:05 -0400

    Updates

---
+++ precache-manifest.40e0395e117f597e7bf5c11325c0cbc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456e67f9ff50c1eb86f660993e11ad6b",
+    "url": "/reactTest/index.html"
+  },
+  {
+    "revision": "1d4426f7845ddfe49030",
+    "url": "/reactTest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d6e5118a6a0c73f83896",
+    "url": "/reactTest/static/js/2.7cda1161.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactTest/static/js/2.7cda1161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d4426f7845ddfe49030",
+    "url": "/reactTest/static/js/main.726fa05b.chunk.js"
+  },
+  {
+    "revision": "7b1c92fe1a56b5695696",
+    "url": "/reactTest/static/js/runtime-main.f0a6a73d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
